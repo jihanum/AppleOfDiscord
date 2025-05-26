039ee3fce23bd340504ad2d8a2c8017fb11452fa
Ji Han <jihanum2009@outlook.com>
2025-05-25 19:27:02 -0700

    Draft 2.2
    Added all section templates in html
    
    Added css styling for about
    
    Added css styling for appearances
    
    Temporary extra whitespace in index.css to divide between sections.

--- js/scrollEffects.js
+++ js/scrollEffects.js
@@ -2,7 +2,7 @@ window.addEventListener("scroll", function () {
   const scrollPosition = window.scrollY;
 
   // Dynamically update the background position
-  if (window.innerWidth < 768) {
+  if (window.innerWidth < 1000) {
     document.getElementById("titlePage").style.backgroundPositionY = `${100 + scrollPosition / -10}%`;
   } else {
     document.getElementById("titlePage").style.backgroundPositionY = `${100 + scrollPosition / -50}%`;
