b3be57d55d7c62e113c94da1b2f4aca9d480fa0b
Ji Han <104695617+jihanum@users.noreply.github.com>
2025-05-30 12:09:12 -0700

    Update scrollEffects.js
    Fix pt1

--- js/scrollEffects.js
+++ js/scrollEffects.js
@@ -1,30 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
-  let sections = document.querySelectorAll('section');
-  let scrollTimeout;
 
-  window.addEventListener('scroll', () => {
-    if (scrollTimeout) clearTimeout(scrollTimeout);
-
-    scrollTimeout = setTimeout(() => {
-      let top = window.scrollY;
-      let windowHeight = window.innerHeight;
-
-      sections.forEach(sec => {
-        let sectionTop = sec.offsetTop;
-        let sectionBottom = sec.offsetTop + sec.offsetHeight;
-
-        // Check if the section is in the viewport
-        if (top + windowHeight > sectionTop && top < sectionBottom) {
-          sec.classList.add('show-animation'); // Show the section
-        } else {
-          sec.classList.remove('show-animation'); // Hide the section
-        }
-      });
-    }, 15);
-  });
-});
-
-/*
 window.addEventListener("scroll", function () {
   const scrollPosition = window.scrollY;
 
@@ -35,4 +9,3 @@ window.addEventListener("scroll", function () {
     document.getElementById("titlePage").style.backgroundPositionY = `${100 + scrollPosition / -50}%`;
   }
 });
-*/
