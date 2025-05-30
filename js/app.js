a2f4f8479407be2ba2af51bc1b69221e83f47098
Ji Han <104695617+jihanum@users.noreply.github.com>
2025-05-30 12:18:47 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,33 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     scrollTimeout = setTimeout(() => {
       let top = window.scrollY;
-
-      sections.forEach(sec => {
-        let offset = sec.offsetTop - window.innerHeight * 0.85;
-        let offset2 = sec.offsetTop - window.innerHeight * 0.15;
-        let height = sec.offsetHeight;
-
-        if ((top >= offset && top < offset + height) || (top >= offset2 && top < offset2 + height)) {
-          sec.classList.add('show-animation');
-        }
-      });
-    }, 15);
-  });
-});
-
-
-
-// add offset2 condition
-
-document.addEventListener('DOMContentLoaded', () => {
-  let sections = document.querySelectorAll('section');
-  let scrollTimeout;
-
-  window.addEventListener('scroll', () => {
-    if (scrollTimeout) clearTimeout(scrollTimeout);
-
-    scrollTimeout = setTimeout(() => {
-      let top = window.scrollY;
       let windowHeight = window.innerHeight;
 
       sections.forEach(sec => {
